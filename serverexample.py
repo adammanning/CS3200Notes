b7bfe8cce046c182455483318e4a53bcf615e189
Adam Manning <adammanning94@gmail.com>
2017-09-12 15:46:33 -0600

    Finished in-class server

--- serverexample.py
+++ serverexample.py
@@ -2,8 +2,23 @@ from http.server import BaseHTTPRequestHandler, HTTPServer # Importing two class
 
 # Create subclass of BaseHTTPRequestHandler
 # Will be instantiated with EVERY request and then dies after response is given
+
+# Always in this order
+# 1 Send response
+# 2 Any headers
+# 3 End headers
+# 4 Wirte the body
+
 class MyHandler(BaseHTTPRequestHandler):
-    pass
+    def do_GET(self):
+        self.send_response(200) # Actually sends the request status
+        self.send_header("Content-Type", "text/html") # "text/html" is a MIME type
+        self.end_headers() # Tell python when your done with headers, even if there aren't any
+        self.wfile.write(bytes("<strong>Hello<strong>", "utf-8")) # Create body (Text, character set)
+        # self.wfile is a file object. Not writing to a file, but the buffering benefits
+        # of a file object can be applied here. Response body
+
+        #self.path to access path string
 
 def main():
     listen = ("127.0.0.1", 8080) # (Host, Port)
