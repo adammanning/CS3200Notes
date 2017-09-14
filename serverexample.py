c784c5c2db23a3fd133c3bbcd6cdb870e073a0a2
Adam Manning <adammanning94@gmail.com>
2017-09-14 15:46:18 -0600

    Implemented do_POST()

--- serverexample.py
+++ serverexample.py
@@ -12,16 +12,49 @@ from http.server import BaseHTTPRequestHandler, HTTPServer # Importing two class
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200) # Actually sends the request status
-        self.send_header("Content-Type", "text/html") # "text/html" is a MIME type
-        self.end_headers() # Tell python when your done with headers, even if there aren't any
-        self.wfile.write(bytes("<strong>Hello<strong>", "utf-8")) # Create body (Text, character set)
+
+
+        print("GET PATH: ", self.path)
+
+        if self.path == "/pandas":
+            self.send_header("Content-Type", "text/html") # "text/html" is a MIME type
+            self.end_headers() # Tell python when your done with headers, even if there aren't any
+            self.wfile.write(bytes("<strong>PANDAS!!</strong>", "utf-8"))
+
+        elif self.path == "/potatoes":
+            self.send_header("Content-Type", "text/html") # "text/html" is a MIME type
+            self.end_headers() # Tell python when your done with headers, even if there aren't any
+            self.wfile.write(bytes("<strong>POTATOES!!</strong>", "utf-8"))
+
+        else:
+            self.send_header("Content-Type", "text/html") # "text/html" is a MIME type
+            self.end_headers() # Tell python when your done with headers, even if there aren't any
+            self.wfile.write(bytes("<strong>Hello<strong>", "utf-8")) # Create body (Text, character set)
+
         # self.wfile is a file object. Not writing to a file, but the buffering benefits
         # of a file object can be applied here. Response body
 
         #self.path to access path string
 
+    def do_POST(self):
+        print("POST PATH: ", self.path)
+
+        if self.path == "/potatoes":
+            length = self.headers["Content-Length"] # Number of bytes inside body of request
+            length = int(length) # length is originally a string
+            print("Content-Length: ", length)
+
+            body = self.rfile.read(length).decode("utf-8")
+            print("REQUEST BODY: ", body)
+            # parse_qs() import from a module
+            
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<strong>POST DONE</strong>", "utf-8"))
+
 def main():
-    listen = ("127.0.0.1", 8080) # (Host, Port)
+    listen = ("0.0.0.0", 8080) # (Host, Port)
     server = HTTPServer(listen, MyHandler) # Instantiate the server
 
     print("Listening...") # Print before serve_forever to know it is working
